561b1eed2c8aff227c5bfc563f232f448acdf8cd
harivardhan <harivardhan.qa@gmail.com>
2016-06-02 19:33:01 +0530

    project is added

---
+++ src/main/java/naukri/update/App.java
@@ -0,0 +1,13 @@
+package naukri.update;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
